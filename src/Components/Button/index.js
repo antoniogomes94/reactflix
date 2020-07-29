4a59e041495f9939bec3846db2c4d7d661327643
Antonio Gomes <antoniogomes313@outlook.com>
2020-07-29 18:17:28 -0300

    Aula 2 Imersão React Alura

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,19 +1,19 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Button = styled.button`
-    color: var(--white);
-    border: 1px solid var(--white);
-    box-sizing: border-box;
-    cursor: pointer;
-    padding: 16px 24px;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 16px;
-    outline: none;
-    border-radius: 5px;
-    text-decoration: none;
-    display: inline-block;
-    transition: opacity .3s;
+  color: var(--white);
+  border: 1px solid var(--white);
+  box-sizing: border-box;
+  cursor: pointer;
+  padding: 16px 24px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  outline: none;
+  border-radius: 5px;
+  text-decoration: none;
+  display: inline-block;
+  transition: opacity .3s;
 
   &:hover,
   &:focus {
@@ -21,4 +21,4 @@ const Button = styled.button`
   }
 `;
 
-export default Button;
+export default Button
