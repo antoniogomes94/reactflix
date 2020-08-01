10c2941b6e62819e6f022ba79361a1f3b5693065
Antonio Gomes <antoniogomes313@outlook.com>
2020-07-31 23:49:34 -0300

    teste Heroku

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
+import Menu from '../../components/Menu';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
